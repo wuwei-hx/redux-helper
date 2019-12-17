5b28e64c25568164b1808598a74e142479b8ba39
David Wu <55110205+wuwei-hx@users.noreply.github.com>
2019-12-17 23:51:12 +0800

    createAction

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import createAsyncActions from "./createAsyncActions";
 import createEntityReducer from "./createEntityReducer";
 import createErrorReducer from "./createErrorReducer";
 import createIsFetchingReducer from "./createIsFetchingReducer";
+import createAction from "./createAction";
 
 export {
   createAsyncActions,
   createEntityReducer,
   createErrorReducer,
-  createIsFetchingReducer
+  createIsFetchingReducer,
+  createAction
 };
